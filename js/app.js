7b46d548b5bb7984eff62d7b6323dd8bd91ec18e
eliza29 <elizaseguraf@gmail.com>
2018-02-15 17:10:04 -0500

    homeworld

--- js/app.js
+++ js/app.js
@@ -18,17 +18,33 @@ window.onload = () => {
   function addDataCharacters() {
     const data = JSON.parse(this.responseText);
     console.log(data);
-    $('#exampleModalLongTitle').html(data.name); 
+    $('#exampleModalLongTitle').html(data.name);
     $('#gender').html(data.gender);
-    $('#home-world').html(data.homeworld); 
+    // $('#home-world').html(data.homeworld);
     $('#birth-year').html(data.birth_year);
     $('#height').html(data.height);
-    $('#mass').html(data.mass); 
-    $('#eye').html(data.eye_color); 
+    $('#mass').html(data.mass);
+    $('#eye').html(data.eye_color);
     $('#hair').html(data.hair_color);
     $('#skin').html(data.skin_color);
     $('#insert-img').attr('src', 'assets/images/' + idImg + '.png');
-    // $('#films').html() = data.films;
+
+
+    // obteniendo datos de homeworld 
+    let newRequest = new XMLHttpRequest();
+    newRequest.open('GET', data.homeworld);
+    newRequest.onload = getExtraData;
+    newRequest.onerror = handleError;
+    newRequest.send();
+
+    function getExtraData() {
+      const dataHome = JSON.parse(this.responseText);
+      const resultHome = dataHome.name;
+      console.log(resultHome);
+      $('#home-world').html(resultHome);
+
+    }
+    
   }
 
   var images = $('#people-container figure img');
